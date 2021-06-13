1ae37371b387026ea03b8af9ec7d2815c42a2b4d
WangZT01 <18640096737@163.com>
2021-06-14 00:13:47 +0800

    Finish all Test.

--- src/clientFile/RMIClient1.java
+++ src/clientFile/RMIClient1.java
@@ -26,9 +26,12 @@ public class RMIClient1 {
             try {
                 String ManagerID;
                 int ManagerInput;
+                boolean ManagerValid = false;
+
                 Scanner ManagerScanner = new Scanner(System.in);
                 System.out.println("Please input the ManagerID. Enter 0, if you want exit.");
                 ManagerID = ManagerScanner.nextLine();
+                ManagerID = ManagerID.toUpperCase();
                 Manager manager = new Manager();
                 manager.setManagerID(ManagerID);
                 rmiCenterServer r_Interface = null;
@@ -36,165 +39,166 @@ public class RMIClient1 {
                 if(ManagerID.equals("0")){
                     break;
                 }
-
                 if (ManagerID.startsWith("MTL")) {
-
+                    ManagerValid = true;
                     Registry registry = LocateRegistry.getRegistry("localhost");
                     r_Interface = (rmiCenterServer) Naming.lookup("rmi://localhost:6231/r_Interface");
 
                 } else if (ManagerID.startsWith("LVL")) {
-
+                    ManagerValid = true;
                     Registry registry = LocateRegistry.getRegistry("localhost");
                     r_Interface = (rmiCenterServer) Naming.lookup("rmi://localhost:6232/r_Interface");
 
                 } else if (ManagerID.startsWith("DDO")) {
-
+                    ManagerValid = true;
                     Registry registry = LocateRegistry.getRegistry("localhost");
                     r_Interface = (rmiCenterServer) Naming.lookup("rmi://localhost:6233/r_Interface");
 
+                }else{
+                    System.out.println("ManagerID is invalid, please try again.");
+                    ManagerValid = false;
                 }
 
+                if(ManagerValid) {
+                    do {
+                        int out = 1;
+                        System.out.println("Welcome: " + ManagerID);
+                        System.out.println("Please select the function.");
+                        System.out.println(
+                                "1.Create Teacher Record. " + "\n" +
+                                        "2.Create Student Record." + "\n" +
+                                        "3.Get Record Counts." + "\n" +
+                                        "4. Edit Record." + "\n" +
+                                        "5. Print Record." + "\n" +
+                                        "0. EXIT");
+                        ManagerInput = ManagerScanner.nextInt();
 
-                do {
+                        switch (ManagerInput) {
 
-                    int out = 1;
-                    System.out.println("Welcome: " + ManagerID);
-                    System.out.println("Please select the function.");
-                    System.out.println(
-                            "1.Create Teacher Record. " + "\n" +
-                                    "2.Create Student Record." + "\n" +
-                                    "3.Get Record Counts." + "\n" +
-                                    "4. Edit Record." + "\n" +
-                                    "5. Print Record." + "\n" +
-                                    "0. EXIT");
-                    ManagerInput = ManagerScanner.nextInt();
+                            case 0:
+                                out = 0;
+                                break;
+                            case 1:
+                                System.out.println("Please Create Teacher Record.");
+                                System.out.println("Enter: lastName, firstName, address, phone, specialization, location(mtl,lvl,ddo) ");
+                                String firstName = ManagerScanner.next();
+                                String lastName = ManagerScanner.next();
+                                String address = ManagerScanner.next();
+                                String phone = ManagerScanner.next();
+                                String specialization = ManagerScanner.next();
 
-                    switch (ManagerInput) {
+                                String location = ManagerScanner.next();
+                                while (!(location.equals("mtl") || location.equals("lvl") || location.equals("ddo"))) {
+                                    System.out.println("Location is invalid, please try again.(mtl,lvl,ddo)\n");
+                                    location = ManagerScanner.next();
+                                }
+                                boolean result = r_Interface.createTRecord(ManagerID, firstName, lastName, address, phone, specialization, location);
+                                if (result) {
+                                    System.out.println("success!");
 
-                        case 0:
-                            out = 0;
+                                    String writeInLog = "Create Teacher Record." + "\n" +
+                                            "Name: " + firstName + " " + lastName + "\n" +
+                                            "Address: " + address + " " + "\n" +
+                                            "Phone: " + phone + " " + "\n" +
+                                            "Specialization: " + specialization + " " + "\n" +
+                                            "Location: " + location + " " + "\n" +
+                                            "Time: " + getTime() + " " + "\n" + "\n";
+
+                                    manager.writeLog(writeInLog);
+                                } else {
+                                    System.out.println("access deny.");
+                                }
+                                break;
+
+                            case 2:
+                                System.out.println("Please Create Student Record.");
+                                System.out.println("Enter: lastName, firstName, CoursesRegister, Status(active/inactive), StatusDate");
+                                firstName = ManagerScanner.next();
+                                lastName = ManagerScanner.next();
+                                String CoursesRegister = ManagerScanner.next();
+                                String Status = ManagerScanner.next();
+                                while (!(Status.equals("active") || Status.equals("inactive"))) {
+                                    System.out.println("Status is invalid, please try again.(active/inactive)\n");
+                                    Status = ManagerScanner.next();
+                                }
+                                String StatusDate = ManagerScanner.next();
+
+                                result = r_Interface.createSRecord(ManagerID, firstName, lastName, CoursesRegister, Status, StatusDate);
+                                if (result) {
+                                    System.out.println("success!");
+
+                                    String writeInLog = "Create Student Record." + "\n" +
+                                            "Name: " + firstName + " " + lastName + "\n" +
+                                            "CoursesRegister: " + CoursesRegister + " " + "\n" +
+                                            "Status: " + Status + " " + "\n" +
+                                            "StatusDate: " + StatusDate + " " + "\n" +
+                                            "Time: " + getTime() + " " + "\n" + "\n";
+
+                                    manager.writeLog(writeInLog);
+
+                                } else {
+                                    System.out.println("access deny.");
+                                }
+                                break;
+
+
+                            case 3:
+
+                                getCount("DDO");
+                                getCount("LVL");
+                                getCount("MTL");
+
+                                break;
+
+                            case 4:
+                                System.out.println("------------------------------------------------------");
+                                System.out.println("Please Input the RecordID, fieldName and the newValue.");
+                                System.out.println("------------------------------------------------------");
+                                System.out.println("The Teacher Record: address, phone, specialization, location(mtl,lvl,ddo) can be changed");
+                                System.out.println("------------------------------------------------------");
+                                System.out.println("The Student Record: coursesregistered, status(active/inactive), statusdate can be changed");
+                                System.out.println("------------------------------------------------------");
+                                System.out.println("Enter: RecordID, fieldName, newValue,");
+                                System.out.println("------------------------------------------------------");
+
+                                String RecordID = ManagerScanner.next();
+                                String fieldName = ManagerScanner.next();
+                                String newValue = ManagerScanner.next();
+
+                                result = r_Interface.editRecord(ManagerID, RecordID, fieldName, newValue);
+                                if (result) {
+                                    System.out.println("success!");
+                                    String writeInLog = "Edit Record." + "\n" +
+                                            "RecordID: " + RecordID + "\n" +
+                                            "fieldName: " + fieldName + " " + "\n" +
+                                            "newValue: " + newValue + " " + "\n" +
+                                            "Time: " + getTime() + " " + "\n" + "\n";
+
+                                    manager.writeLog(writeInLog);
+
+                                } else {
+                                    System.out.println("access deny.");
+                                }
+                                break;
+
+                            case 5:
+                                result = r_Interface.printRecord(ManagerID);
+                                if (result) {
+                                    System.out.println("success!");
+
+                                } else {
+                                    System.out.println("access deny.");
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+
+                        if (out == 0) {
                             break;
-                        case 1:
-                            System.out.println("Please Create Teacher Record.");
-                            System.out.println("Enter: lastName, firstName, address, phone, specialization, location(mtl,lvl,ddo) ");
-                            String firstName = ManagerScanner.next();
-                            String lastName = ManagerScanner.next();
-                            String address = ManagerScanner.next();
-                            String phone = ManagerScanner.next();
-                            String specialization = ManagerScanner.next();
-
-                            String location = ManagerScanner.next();
-                            while (!(location.equals("mtl") || location.equals("lvl") || location.equals("ddo"))) {
-                                System.out.println("Location is invalid, please try again.(mtl,lvl,ddo)\n");
-                                location = ManagerScanner.next();
-                            }
-                            boolean result = r_Interface.createTRecord(ManagerID, firstName, lastName, address, phone, specialization, location);
-                            if (result) {
-                                System.out.println("success!");
-
-                                String writeInLog = "Create Teacher Record." + "\n" +
-                                        "Name: " + firstName + " " + lastName + "\n" +
-                                        "Address: " + address + " " + "\n" +
-                                        "Phone: " + phone + " " + "\n" +
-                                        "Specialization: " + specialization + " " + "\n" +
-                                        "Location: " + location + " " + "\n" +
-                                        "Time: " + getTime() + " " + "\n" + "\n";
-
-                                manager.writeLog(writeInLog);
-                            } else {
-                                System.out.println("access deny.");
-                            }
-                            break;
-
-                        case 2:
-                            System.out.println("Please Create Student Record.");
-                            System.out.println("Enter: lastName, firstName, CoursesRegister, Status(active/inactive), StatusDate");
-                            firstName = ManagerScanner.next();
-                            lastName = ManagerScanner.next();
-                            String CoursesRegister = ManagerScanner.next();
-                            String Status = ManagerScanner.next();
-                            while (!(Status.equals("active") || Status.equals("inactive"))) {
-                                System.out.println("Status is invalid, please try again.(active/inactive)\n");
-                                Status = ManagerScanner.next();
-                            }
-                            String StatusDate = ManagerScanner.next();
-
-                            result = r_Interface.createSRecord(ManagerID, firstName, lastName, CoursesRegister, Status, StatusDate);
-                            if (result) {
-                                System.out.println("success!");
-
-                                String writeInLog = "Create Student Record." + "\n" +
-                                        "Name: " + firstName + " " + lastName + "\n" +
-                                        "CoursesRegister: " + CoursesRegister + " " + "\n" +
-                                        "Status: " + Status + " " + "\n" +
-                                        "StatusDate: " + StatusDate + " " + "\n" +
-                                        "Time: " + getTime() + " " + "\n" + "\n";
-
-                                manager.writeLog(writeInLog);
-
-                            } else {
-                                System.out.println("access deny.");
-                            }
-                            break;
-
-
-                        case 3:
-
-                            getCount("DDO");
-                            getCount("LVL");
-                            getCount("MTL");
-
-                            break;
-
-                        case 4:
-                            System.out.println("------------------------------------------------------");
-                            System.out.println("Please Input the RecordID, fieldName and the newValue.");
-                            System.out.println("------------------------------------------------------");
-                            System.out.println("The Teacher Record: address, phone, specialization, location(mtl,lvl,ddo) can be changed");
-                            System.out.println("------------------------------------------------------");
-                            System.out.println("The Student Record: coursesregistered, status(active/inactive), statusdate can be changed");
-                            System.out.println("------------------------------------------------------");
-                            System.out.println("Enter: RecordID, fieldName, newValue,");
-                            System.out.println("------------------------------------------------------");
-
-                            String RecordID = ManagerScanner.next();
-                            String fieldName = ManagerScanner.next();
-                            String newValue = ManagerScanner.next();
-
-                            result = r_Interface.editRecord(ManagerID, RecordID, fieldName, newValue);
-                            if (result) {
-                                System.out.println("success!");
-                                String writeInLog = "Edit Record." + "\n" +
-                                        "RecordID: " + RecordID + "\n" +
-                                        "fieldName: " + fieldName + " " + "\n" +
-                                        "newValue: " + newValue + " " + "\n" +
-                                        "Time: " + getTime() + " " + "\n" + "\n";
-
-                                manager.writeLog(writeInLog);
-
-                            } else {
-                                System.out.println("access deny.");
-                            }
-                            break;
-
-                        case 5:
-                            result = r_Interface.printRecord(ManagerID);
-                            if (result) {
-                                System.out.println("success!");
-
-                            } else {
-                                System.out.println("access deny.");
-                            }
-                            break;
-                        default:
-                            break;
-                    }
-
-                    if (out == 0){
-                        break;
-                    }
-                } while (true);
-
+                        }
+                    } while (true);
+                }
             } catch (RemoteException e) {
                 e.printStackTrace();
             } catch (NotBoundException e) {
@@ -202,6 +206,7 @@ public class RMIClient1 {
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
+
         }
     }
 
